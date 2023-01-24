65d8c564e62f0a495bb13da21f726e26816dcfa8
steven <alvaradosteven315@gmail.com>
2023-01-23 22:31:13 -0500

    creating structure

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const authRouter = require('./routes/auth.routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'welcome to the server' });
+});
+
+app.use('/api/v1/auth', authRouter);
+
+module.exports = app;
---
+++ src/services/auth.services.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const models = require('../models');
+
+const { users } = models;
+
+class AuthServices {
+  static async register(user) {
+    try {
+      const result = await users.create(user);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async login(credentials) {
+    try {
+      const { email, password } = credentials;
+      const user = await users.findOne({
+        where: { email },
+      });
+      if (user) {
+        const isValid = bcrypt.compareSync(password, user.password);
+        return isValid ? { isValid, user } : { isValid };
+      }
+      return { isValid: false };
+    } catch (error) {
+      throw error;
+    }
+  }
+  static genToken(data) {
+    try {
+      const token = jwt.sign(data, process.env.JWT_SECRET, {
+        // expiresIn: '10m',
+        algorithm: 'HS512',
+      });
+      return token;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = AuthServices;

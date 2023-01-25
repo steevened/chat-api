e04aed5269b41011a516d6c59aef6034baaa0257
steven <alvaradosteven315@gmail.com>
2023-01-24 22:23:11 -0500

    generating documentation with swagger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-const app = require('./app')
-require('dotenv').config()
+const app = require('./app');
+require('dotenv').config();
+const swaggerDocs = require('../swagger');
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+  swaggerDocs(app, PORT);
+});
---
+++ swagger.js
@@ -0,0 +1,34 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+require('dotenv').config();
+
+const options = {
+  apis: [],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'chat with nodejs',
+      version: '0.0.9',
+      description: 'API for chat app',
+    },
+  },
+};
+
+//specification json for our documentation
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerDocs = (app, port) => {
+  //generate the route to show the documentation
+  app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+  app.get('/api/v1/docs.json', (req, res) => {
+    res.setHeader({ 'Content-Type': 'application/json' });
+    res.send(swaggerSpec);
+  });
+
+  console.log(
+    `Swagger docs available at ${process.env.URL}:${port}/api/v1/docs`
+  );
+};
+
+module.exports = swaggerDocs;
